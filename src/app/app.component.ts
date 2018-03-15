c2fe1204457ba350e0e4fee67ccee604aabeff9b
njsohi <nileshjoshi.bn@gmail.com>
2018-03-14 20:52:52 -0500

    test1 test new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,4 @@ import {ConfirmDialogModule} from 'primeng/confirmdialog';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private confirmationService: ConfirmationService) {}
-  message:string;
-    confirm() {
-        this.confirmationService.confirm({
-            message: 'Are you sure that you want to perform this action?',
-            accept: () => {
-                //Actual logic to perform a confirmation
-                this.message="yes"
-            },reject: () => {
-                this.message="no"
-            }
-        });
-    }
-
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-  }
-
-  onSubmit(value:NgForm){
-    console.log(value);
-  }
 }
