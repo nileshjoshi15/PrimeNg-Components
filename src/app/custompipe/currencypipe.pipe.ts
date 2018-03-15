accf2fc09ba6e58f91cfdf3d32f0c902318dda71
njsohi <nileshjoshi.bn@gmail.com>
2018-03-15 08:40:19 -0500

    change currency pipe

--- src/app/custompipe/currencypipe.pipe.ts
+++ src/app/custompipe/currencypipe.pipe.ts
@@ -7,11 +7,9 @@ import { CurrencyPipe } from '@angular/common';
 export class CurrencypipePipe extends CurrencyPipe {
 
   transform(value: any): string {
-      if(value && value.length === 1){
+      if(value && value.length >0){
+        value = value.length === 1 ? value : value.substring(1)
         return super.transform(value, 'USD',true,'1.0-2');
-      }else if(value && value.length>1){
-        return value;
       }
-
   }
 }
